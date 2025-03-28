55aecd02d3182e5f0462d14fbc340cbb5e044114
Froldas <jan.froldas@gmail.com>
2025-03-28 14:24:26 +0100

    Update commends in encoding abclass

--- src/encoding.py
+++ src/encoding.py
@@ -6,20 +6,30 @@ class Encoding(ABC):
     """
     Abstract Interface which encoding classes must implement
     """
+
     @property
     @abstractmethod
     def letters(self) -> [str]:
-        """This must be implemented by subclasses"""
+        """
+        List of all the letters recognized from the dataset
+        This property must be implemented by subclasses
+        """
         pass
 
     @property
     @abstractmethod
     def letter_encoding(self) -> dict[str, [Any]]:
-        """This must be implemented by subclasses"""
+        """
+        Dictionary mapping all letters to an input vector
+        This property must be implemented by subclasses
+        """
         pass
 
     @property
     @abstractmethod
     def encoding_size(self) -> int:
-        """This must be implemented by subclasses"""
+        """
+        Size of the encoding per letter
+        This property must be implemented by subclasses
+        """
         pass
